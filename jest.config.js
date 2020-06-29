45fc3ba37e0e6f8eef196c60605f73024367cfaa
Danilo Augusto <daniloaugustogrs@gmail.com>
2020-06-29 18:51:27 -0300

    chore: update jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
